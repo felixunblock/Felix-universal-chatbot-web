649220e762f66e67f4055bc22c891571b3dce78a
Felix Weng <felixweng@Felixs-MacBook.local>
2026-07-06 19:08:25 -0500

    Phase 3: per-org branding tokens (fonts, theme, gradient, logos, tagline, footer, support)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3065491398" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "223058385" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
